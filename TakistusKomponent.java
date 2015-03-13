26434d82bffc784b8a969e70ebe4d4dd8c4024b0
ophyneus <ophyneus@tlu.ee>
2015-03-13 10:58:11 +0200

    Listai TakistusKomponent

--- TakistusKomponent.java
+++ TakistusKomponent.java
@@ -1,5 +1,10 @@
 abstract class TakistusKomponent{
 	abstract double kysiTakistus();
+	/**
+	*	Leiatakse etteantud pingele vastav vool
+	*	@param pinge - rakendatav pinge vooltides
+	*/
+	
 	public double vool(double pinge){
 		return pinge/kysiTakistus();
 	}
