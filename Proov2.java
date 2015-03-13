3362f67a728526d7d76718814db6799ad04fa831
ophyneus <ophyneus@tlu.ee>
2015-03-13 11:27:16 +0200

    Lisatud Takistus class

---
+++ Proov2.java
@@ -0,0 +1,6 @@
+public class Proov2{
+	public static void main(String[] arg){
+		TakistusKomponent t = new Takisti(300);
+		System.out.println("Vool 9 vooldi korral:  "+t.vool(9));
+	}
+}
---
+++ Takisti.java
@@ -0,0 +1,11 @@
+public class Takisti extends TakistusKomponent{
+	private double t;
+	
+	public Takisti (double takistus){
+		t = takistus;
+	}
+	
+	public double  kysiTakistus(){
+		return t;
+	};
+} 
