9a5bf10e44b84c6d5e96c2d739aa96a192d1da57
alexn <alexn@greeny.cs.tlu.ee>
2015-03-24 11:21:47 +0200

    proov2

--- Proov2.java
+++ Proov2.java
@@ -1,6 +1,8 @@
 public class Proov2{
 	public static void main(String[] arg){
-		TakistusKomponent t = new Takisti(302);
-		System.out.println("Vool 9 vooldi korral:  "+t.vool(9));
+		TakistusKomponent t = new Takisti(300);
+		Vooluallikas telefonilaadija = new Laadija(5,0.5);
+		telefonilaadija.yhendaTarbija(t);
+		System.out.println(telefonilaadija.kysiVool());
 	}
 }
